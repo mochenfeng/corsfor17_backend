6191223d4b3eab656bfe86504805f3b29e1ca623
Administrator <1>
2018-12-28 09:12:15 +0800

    1

---
+++ profile.php
@@ -0,0 +1,7 @@
+<?php
+header("Access-Control-Allow-Origin:http://10.30.29.100:63341");//前端ip和端口
+header("Access-Control-Allow-Credentials:true");
+header('Access-Control-Allow-Headers:Content-Type');
+$userName=array("userName"=>'123456');
+echo json_encode($userName);
+?>
